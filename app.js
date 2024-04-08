4492ec94c8cd7d5981cb295f9d73a5cfe53bbd71
Rui Marques <a.marques1967@gmail.com>
2024-04-09 00:46:37 +0100

    Added the suggested locations

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ import { AVAILABLE_LOCATIONS } from "./data/available-locations.js";
 import renderLocationsPage from "./views/index.js";
 import renderLocation from "./views/components/location.js";
 
+function getSuggestedLocations() {
+  const availableLocations = AVAILABLE_LOCATIONS.filter(
+    (location) => !INTERESTING_LOCATIONS.includes(location)
+  );
+
+  if (availableLocations.length < 2) return availableLocations;
+
+  const suggestedLocation1 = availableLocations.splice(
+    Math.floor(Math.random() * availableLocations.length),
+    1
+  )[0];
+  const suggestedLocation2 = availableLocations.splice(
+    Math.floor(Math.random() * availableLocations.length),
+    1
+  )[0];
+
+  return [suggestedLocation1, suggestedLocation2];
+}
+
 const app = express();
 
 const INTERESTING_LOCATIONS = [];
@@ -15,7 +34,16 @@ app.get("/", (req, res) => {
   const availableLocations = AVAILABLE_LOCATIONS.filter(
     (location) => !INTERESTING_LOCATIONS.includes(location)
   );
-  res.send(renderLocationsPage(availableLocations, INTERESTING_LOCATIONS));
+
+  const suggestedLocations = getSuggestedLocations();
+
+  res.send(
+    renderLocationsPage(
+      suggestedLocations,
+      availableLocations,
+      INTERESTING_LOCATIONS
+    )
+  );
 });
 
 app.post("/places", (req, res) => {
@@ -27,8 +55,14 @@ app.post("/places", (req, res) => {
     (location) => !INTERESTING_LOCATIONS.includes(location)
   );
 
+  const suggestedLocations = getSuggestedLocations();
+
   res.send(`
     ${renderLocation(location, false)}
+
+    <ul class="locations" id="suggested-locations" hx-swap-oob="true">
+      ${suggestedLocations.map((location) => renderLocation(location)).join("")}
+    </ul>
 
     <ul id="available-locations" class="locations" hx-swap-oob="true">
       ${availableLocations.map((location) => renderLocation(location)).join("")}
--- views/index.js
+++ views/index.js
@@ -1,6 +1,10 @@
-import renderLocation from './components/location.js';
+import renderLocation from "./components/location.js";
 
-export default function renderLocationsPage(availableLocations, interestingLocations) {
+export default function renderLocationsPage(
+  suggestedLocations,
+  availableLocations,
+  interestingLocations
+) {
   return `
     <!DOCTYPE html>
     <html>
@@ -21,17 +25,30 @@ export default function renderLocationsPage(availableLocations, interestingLocat
           </p>
         </header>
         <main>
+          <section id="suggested-locations-section">
+            <h2>Currently suggested</h2>
+            <ul class="locations" id="suggested-locations">
+              ${suggestedLocations
+                .map((location) => renderLocation(location))
+                .join("")}
+            </ul>
+          </section>
+
           <section id="int-locations-section" class="locations-category">
             <h2>My Dream Locations</h2>
             <ul id="interesting-locations" class="locations">
-              ${interestingLocations.map((location) => renderLocation(location, false)).join('')}
+              ${interestingLocations
+                .map((location) => renderLocation(location, false))
+                .join("")}
             </ul>
           </section>
 
           <section class="locations-category">
             <h2>Available Locations</h2>
             <ul id="available-locations" class="locations">
-              ${availableLocations.map((location) => renderLocation(location)).join('')}
+              ${availableLocations
+                .map((location) => renderLocation(location))
+                .join("")}
             </ul>
           </section>
         </main>
