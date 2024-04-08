3378e7417dcd88600a3e5a0626a06606bc2b18ec
Rui Marques <a.marques1967@gmail.com>
2024-04-09 00:05:18 +0100

    Adding and removing items to dream locations

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-import express from 'express';
+import express from "express";
 
-import { AVAILABLE_LOCATIONS } from './data/available-locations.js';
-import renderLocationsPage from './views/index.js';
-import renderLocation from './views/components/location.js';
+import { AVAILABLE_LOCATIONS } from "./data/available-locations.js";
+import renderLocationsPage from "./views/index.js";
+import renderLocation from "./views/components/location.js";
 
 const app = express();
 
 const INTERESTING_LOCATIONS = [];
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const availableLocations = AVAILABLE_LOCATIONS.filter(
     (location) => !INTERESTING_LOCATIONS.includes(location)
   );
   res.send(renderLocationsPage(availableLocations, INTERESTING_LOCATIONS));
 });
 
-app.post('/places', (req, res) => {
+app.post("/places", (req, res) => {
   const locationId = req.body.locationId;
   const location = AVAILABLE_LOCATIONS.find((loc) => loc.id === locationId);
   INTERESTING_LOCATIONS.push(location);
 
-  res.send(`
-    TODO
-  `);
+  res.send(renderLocation(location, false));
 });
 
-app.delete('/places/:id', (req, res) => {
+app.delete("/places/:id", (req, res) => {
   const locationId = req.params.id;
   const locationIndex = INTERESTING_LOCATIONS.findIndex(
     (loc) => loc.id === locationId
